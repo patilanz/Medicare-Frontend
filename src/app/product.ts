16f7c67757cfd734b38c47c7d26e9307ecf67cfa
patilanz <patilanz1@hotmail.com>
2023-08-12 21:36:38 +0200

    Initial commit

---
+++ src/app/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    pid!: number;
+    available!: boolean;
+    name!: string;
+    brand!: string;
+    category!: string;
+    description!: string;
+    salt!: string;
+    totalAvailable!: number;
+    price!: number;
+}
